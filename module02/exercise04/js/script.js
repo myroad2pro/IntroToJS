17abfecfca47a4c65f14fc1361251e6f1fce0586
hoanganh <hoanganhA91013@gmail.com>
2017-11-10 15:32:06 +0700

    Add click and drag to exercise04 of module02

--- module02/exercise04/js/script.js
+++ module02/exercise04/js/script.js
@@ -1,54 +1,63 @@
-var selected = null;    // elements to be moved
-var oldMouseX, oldMouseY;   //stores x and y coordinates of the mouse pointer
+window.onmousemove = moveElem;
+window.onmouseup = stopMovingElem;
+window.onload = init;
+
+var selected = null; // element to be moved
+var oldMouseX, oldMouseY; // Stores x & y coordinates of the mouse pointer
 var elemX, elemY;
 
 function init() {
-    var dragMe = document.querySelector('#dragMe');
-    dragMe.onmousedown = dragInit;
-    window.onmousemove = moveElem;
-    window.onmouseup = stopMovingElem;
+    document.querySelector('#dragMe').onmousedown = function (evt) {
+        dragInit(evt);
+    };
 }
-
+  
+// Will be called when user starts dragging an element
 function dragInit(evt) {
     // Store the elem
     selected = evt.target;
     elemX = selected.offsetLeft;
     elemY = selected.offsetTop;
-
+  
     oldMouseX = evt.clientX;
     oldMouseY = evt.clientY;
 }
 
-function moveElem(evt) {
-    var newMouseX = evt.clientX;
-    var newMouseY = evt.clientY;
-
-    if (oldMouseX !== undefined) {
+// Will be called when user dragging an element
+function moveElem(e) {
+    // new mouse ps
+    var newMouseX = e.clientX;
+    var newMouseY = e.clientY;
+  
+    if(oldMouseX !== undefined) {
         // how many pixels did we move the mouse?
         var dx = newMouseX - oldMouseX;
         var dy = newMouseY - oldMouseY;
+     }
+    
+    if (selected !== null) {  
+        // move the selected element dx, dy pixels hozontally/vertically
+        changePosOfSelectedElement(dx, dy);
     }
-
-    if (selected !== null) {
-        // move the selected element dx, dy pixels horizontally/vertically
-        // update the old position of the selected element
-        elemX += dx;
-        elemY += dy;
-        // change the pos on the screen of the element
-        // by modifying its CSS left/top properties
-        selected.style.left = elemX + 'px !important';
-        selected.style.top = elemY + 'px !important';
-    }
-
+  
     // update the old position of the mouse
     oldMouseX = newMouseX;
     oldMouseY = newMouseY;
 }
 
 function changePosOfSelectedElement(dx, dy) {
-
+  // update the old position of the selected element
+  elemX += dx;
+  elemY += dy;
+  
+  // change the pos on screen of the element
+  // by modifying its CSS left/top properties
+  selected.style.left = elemX + 'px';
+  selected.style.top = elemY + 'px';
 }
 
-function stopMovingElem(evt) {
+// Destroy the object when we are done
+function stopMovingElem() {
     selected = null;
 }
+
